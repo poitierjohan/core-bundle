7edb16e4db3a351c22bb114a491afd2670186380
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2015-08-29 19:34:21 +0200

    Mise à jour de l'administration bootstrap + adminLTE

--- Dywee/CoreBundle/Form/Type/SeoType.php
+++ Dywee/CoreBundle/Form/Type/SeoType.php
@@ -5,16 +5,29 @@ namespace Dywee\CoreBundle\Form\Type;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 
 class SeoType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-        ->add('metaTitle',          'textarea', array('required' => false))
+        ->add('metaTitle',          'text',     array('required' => false))
         ->add('metaDescription',    'textarea', array('required' => false))
         ->add('metaKeywords',       'textarea', array('required' => false))
         ->add('seoUrl',             'text',     array('required' => false));
+
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function(FormEvent $event) {
+            $entity = $event->getData();
+            $form = $event->getForm();
+
+            // Dans le cas où c'est la page d'accueil
+            if ($entity instanceof Dywee\DyweeCMSBundle\Entity\Page){
+                if($entity->getType() == 1)
+                    $form->remove('seoUrl');
+            }
+        });
     }
 
     public function setDefaultOptions(OptionsResolverInterface $resolver)
