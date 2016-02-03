4cd3464cc9dd9ae7c9f9eecaaf28962a3290799e
Olivier Delbruyere <olivier.delbruyere@hotmail.com>
2016-02-03 17:28:05 +0100

    no message

--- Dywee/CoreBundle/Resources/public/js/admin.js
+++ Dywee/CoreBundle/Resources/public/js/admin.js
@@ -13,17 +13,24 @@ $(document).ready(function()
         var route = Routing.generate('inscription_deleteFromAjax', {id: $(this).attr('data-id') });
 
         if (!$('#dataConfirmModal').length)
-            $('body').append('<div class="modal fade" id="dataConfirmModal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Attention!</h4></div><div class="modal-body"><p><i class="fa fa-spinner fa-spin"></i> Veuillez patienter </p></div><div class="modal-footer"><a class="btn btn-danger" id="dataConfirmOK">Supprimer</a><button type="button" class="btn btn-default" data-dismiss="modal">Annuler</button></div></div></div></div>');
+            $('body').append('<div class="modal fade" id="dataConfirmModal" role="dialog" aria-labelledby="dataConfirmLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button><h4 class="modal-title">Attention!</h4></div><div class="modal-body"><p><i class="fa fa-spinner fa-spin"></i> Veuillez patienter </p></div><div class="modal-footer"><a class="btn btn-danger" id="dataConfirmOK">Supprimer</a><button type="button" class="btn btn-default" data-dismiss="modal" id="dataConfirmAboard">Annuler</button></div></div></div></div>');
         $('#dataConfirmModal').find('.modal-body').html('<p>Etes-vous sur de vouloir supprimer cet élément?</p><p>Cette action est irréversible.</p>');
+        $('#dataConfirmAboard').click(function(e){
+            $btn.html('<i class="fa fa-trash-o"></i>');
+        });
+
         $('#dataConfirmOK').click(function(e)
         {
-            $(this).addClass('disabled').html('<i class="fa fa-spinner fa-spin"></i> Veuillez patienter');
+            $confirmBtn = $(this);
+            $confirmBtn.addClass('disabled').html('<i class="fa fa-spinner fa-spin"></i> Veuillez patienter');
             $.post(route , function( data ) {
                 data = JSON.parse(data);
                 if(data.type == "success")
                 {
                     $('#dataConfirmModal').modal('hide');
-                    $btn.closest($btn.attr('data-container')).fadeOut();
+                    $confirmBtn.removeClass('disabled').html('Supprimer');
+                    $btn.html('<i class="fa fa-trash-o"></i>');
+                    $btn.closest($btn.attr('data-container')).fadeOut("slow");
                 }
                 else {
                     $modal = $("#dywee-modal");
